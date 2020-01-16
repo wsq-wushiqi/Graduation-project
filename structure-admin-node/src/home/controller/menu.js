eb35888ef01abc174517b6001bf048c8056fe27f
wushiqi <wushiqi@startdima.com.cn>
2020-01-16 08:53:26 +0800

    增加新增菜单、修改菜单、删除菜单、获取菜单列表接口

---
+++ structure-admin-node/src/home/controller/menu.js
@@ -0,0 +1,56 @@
+const Base = require('./base')
+module.exports = class extends Base {
+  // 获取菜单列表
+  async getPageMenuListAction() {
+    try {
+      let list = await this.model('menu').select()
+      return this.success(list)
+    }
+    catch(e) {
+      console.log(e);
+      return this.fail('获取菜单列表失败')
+    }
+  }
+  // 添加菜单
+  async addMenuAction() {
+    let { m_n_id, m_name, m_p_id, m_url, m_icon } = this.post()
+    try {
+      let mpid = m_p_id === '' ? null : m_p_id
+      let check = await this.model('menu').where({ m_name, m_n_id, m_url, _logic: 'OR'}).select()
+      if (think.isEmpty(check)) {
+        let add = await this.model('menu').add({ m_n_id, m_name, m_p_id: mpid, m_url, m_icon })
+        return this.success(add)
+      } else {
+        return this.fail('添加失败，菜单名称/id/路由不能重复')
+      }
+    }
+    catch(e) {
+      console.log(e);
+      return this.fail('添加菜单失败')
+    }
+  }
+  // 修改菜单
+  async updateMenuAction() {
+    let { m_n_id, m_name, m_p_id, m_url, m_icon } = this.post()
+    try {
+      let update = await this.model('menu').where({ m_n_id }).update({ m_name, m_p_id, m_url, m_icon })
+      return this.success(update)
+    }
+    catch(e) {
+      console.log(e);
+      return this.fail('修改菜单失败')
+    }
+  }
+  // 删除菜单
+  async deleteMenuAction() {
+    let m_n_id = this.post('m_n_id')
+    try {
+      let result = await this.model('menu').where({ m_n_id }).delete()
+      return this.success(result)
+    }
+    catch(e) {
+      console.log(e);
+      return this.fail('删除菜单失败')
+    }
+  }
+}
