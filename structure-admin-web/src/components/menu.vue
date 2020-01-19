d58cc370a344b274c5f12f942955e667d218e1cb
wushiqi <wushiqi@startdima.com.cn>
2020-01-19 11:17:39 +0800

    解决只有父级菜单报错的问题，删除写死菜单的代码

--- structure-admin-web/src/components/menu.vue
+++ structure-admin-web/src/components/menu.vue
@@ -9,7 +9,7 @@
       router = router
       active-text-color="rgb(28,41,89)">
       <template v-for="(item, key) in menuList">
-        <el-submenu :key="key" v-if="item.children.length!==0" :index="item.m_url">
+        <el-submenu :key="key" v-if="item.children && item.children.length!==0" :index="item.m_url">
           <template slot="title">
             <i :class="item.m_icon"></i>
             <span>{{ item.m_name }}</span>
@@ -21,73 +21,13 @@
             </template>
           </el-menu-item>
         </el-submenu>
+        <el-submenu :key="item.m_n_id" v-else :index="item.m_url">
+          <template slot="title">
+            <i :class="item.m_icon"></i>
+            <span>{{ item.m_name }}</span>
+          </template>
+        </el-submenu>
       </template>
-      <!-- <el-submenu index="0">
-        <template slot="title">
-          <i class="el-icon-edit"></i>
-          <span slot="title">培训管理</span>
-        </template>
-        <el-menu-item index="/courseApplication">
-          <i class="el-icon-location"></i>
-          <span slot="title">课程申请</span>
-        </el-menu-item>
-        <el-menu-item index="/courseArrange">
-          <i class="el-icon-location"></i>
-          <span slot="title">课程安排</span>
-        </el-menu-item>
-        <el-menu-item index="/personPlan">
-          <i class="el-icon-location"></i>
-          <span slot="title">个人培训计划</span>
-        </el-menu-item>
-      </el-submenu>
-      <el-submenu index="1">
-        <template slot="title">
-          <i class="el-icon-edit"></i>
-          <span slot="title">评教管理</span>
-        </template>
-        <el-menu-item index="/addEvaluate">
-          <i class="el-icon-location"></i>
-          <span slot="title">学生评教</span>
-        </el-menu-item>
-        <el-menu-item index="/checkEvaluate">
-          <i class="el-icon-location"></i>
-          <span slot="title">评教查看</span>
-        </el-menu-item>
-        <el-menu-item index="/examine">
-          <i class="el-icon-location"></i>
-          <span slot="title">考核管理</span>
-        </el-menu-item>
-        <el-menu-item index="/checkResult">
-          <i class="el-icon-edit"></i>
-          <span slot="title">成绩查看</span>
-        </el-menu-item>
-      </el-submenu>
-      <el-submenu index='2'>
-        <template slot="title">
-          <i class="el-icon-edit"></i>
-          <span slot="title">信息管理</span>
-        </template>
-        <el-menu-item index="/userInfo">
-          <i class="el-icon-location"></i>
-          <span slot="title">个人资料</span>
-        </el-menu-item>
-        <el-menu-item index="/staffInfo">
-          <i class="el-icon-location"></i>
-          <span slot="title">用户信息管理</span>
-        </el-menu-item>
-        <el-menu-item index="/lectureInfo">
-          <i class="el-icon-location"></i>
-          <span slot="title">教师信息管理</span>
-        </el-menu-item>
-        <el-menu-item index="/privilegeManage">
-          <i class="el-icon-edit"></i>
-          <span slot="title">用户管理</span>
-        </el-menu-item>
-        <el-menu-item index="/roleManage">
-          <i class="el-icon-edit"></i>
-          <span slot="title">角色管理</span>
-        </el-menu-item>
-      </el-submenu> -->
     </el-menu>
   </div>
 </template>
